8b6c58e1fa5b49494498567bc5bd9c826a781bc1
harrisonpim <harrison.stroma@gmail.com>
2022-01-29 15:32:27 +0000

    restructure the data and add some warning and failure messages

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const fetch = (...args) =>
 
 function makeUrlAbsolute(url, baseUrl) {
   if (!url.startsWith("http") & !url.startsWith("mailto")) {
-    return baseUrl + url;
+    return (
+      baseUrl.replace(/^\/+|\/+$/g, "") + "/" + url.replace(/^\/+|\/+$/g, "")
+    );
   }
   return url;
 }
@@ -16,10 +18,10 @@ function makeUrlAbsolute(url, baseUrl) {
     const sitemapUrl = core.getInput("sitemap");
 
     // construct the base url from the sitemap url
-    const baseUrl = sitemapUrl.substring(0, sitemapUrl.lastIndexOf("/"));
+    const baseUrl = sitemapUrl.substring(0, sitemapUrl.lastIndexOf("/") + 1);
 
-    const sitemap = await fetch(sitemapUrl).then((res) => res.text());
     // fetch the sitemap and parse a list of URLs from it
+    const sitemap = await fetch(sitemapUrl).then((res) => res.text());
     const urls = [];
     const $ = cheerio.load(sitemap, { xmlMode: true });
     $("loc").each(function () {
@@ -29,39 +31,56 @@ function makeUrlAbsolute(url, baseUrl) {
       }
     });
 
-    // find all links on each of the pages on the sitemap returning an object
-    // with the URL as the key and a list of links as the value
-    const links = {};
-    for (const url of urls) {
-      const page = await fetch(url).then((res) => res.text());
-      const $ = cheerio.load(page);
-      const pageLinks = [];
-      $("a").each(function () {
-        const link = $(this).attr("href");
-        if (!pageLinks.includes(link)) {
-          const absoluteLink = makeUrlAbsolute(link, baseUrl);
-          pageLinks.push(absoluteLink);
-        }
-      });
-      links[url] = pageLinks;
-    }
-
-    // for each page, gather a list of any links which do not return a 200
+    // we'll return an object with each URL from the sitemap keys and a list
+    // of any links which do not return a 200 as the values
     const brokenLinks = {};
-    for (const url in links) {
-      for (const link of links[url]) {
-        const brokenPageLinks = [];
-        if (link.startsWith("http")) {
-          const res = await fetch(link);
-          if (res.status !== 200) {
-            brokenPageLinks.push(link);
+    for (const url of urls) {
+      try {
+        const response = await fetch(url).then((res) => res.text());
+        const $ = cheerio.load(response, { xmlMode: true });
+        const linksOnPage = [];
+        $("a").each(function () {
+          const href = $(this).attr("href");
+          if (href) {
+            linksOnPage.push(makeUrlAbsolute(href, baseUrl));
+          }
+        });
+        const brokenLinksOnPage = [];
+        for (const url of linksOnPage) {
+          if (url.startsWith("http")) {
+            try {
+              const response = await fetch(url);
+              const status = response.status;
+              if (status !== 200) {
+                brokenLinksOnPage.push(url);
+              }
+            } catch (error) {
+              brokenLinksOnPage.push(url);
+            }
           }
         }
-        brokenLinks[url] = brokenPageLinks;
+        brokenLinks[url] = brokenLinksOnPage;
+      } catch (error) {
+        core.warning(`Failed to fetch ${url}`);
+        brokenLinks[url] = [];
       }
     }
 
-    console.log(brokenLinks);
+    // construct a failure message for any links which don't return a 200
+    const failureMessages = [];
+    for (const url in brokenLinks) {
+      if (brokenLinks[url].length > 0) {
+        failureMessages.push(
+          `${url} contains broken links:\n${brokenLinks[url].join(", ")}\n`
+        );
+      }
+    }
+
+    // if there are any broken links, set the action status to failure
+    if (failureMessages.length > 0) {
+      core.setFailed(failureMessages.join("\n"));
+    }
+    
   } catch (error) {
     core.setFailed(error.message);
   }
